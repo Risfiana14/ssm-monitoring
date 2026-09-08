dfbc98604d9c6867437eca2af17c27f891e93e18
Risfiana14 <resfyana15@gmail.com>
2026-09-08 08:34:19 +0700

    update gerbong

--- index.php
+++ index.php
@@ -361,7 +361,7 @@
     <!-- Bootstrap 5 JS Bundle -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script>
-        const uniqueCars = ['K102436', 'K102438', 'K102437', 'K102439', 'M102411', 'K302452'];
+        const uniqueCars = ['K102436', 'K102437', 'K102438', 'K102439', 'K302452', 'M102411'];
         let globalDeviceData = [];
         
         const deviceModalElem = document.getElementById('deviceModal');
